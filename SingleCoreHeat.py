33184999777d61b0911fa217b8012f10b1cb69c8
Alexander Larsson <alexx.1992@gmail.com>
2015-10-18 12:37:50 +0200

    Almost wrote an awesome function

--- SingleCoreHeat.py
+++ SingleCoreHeat.py
@@ -77,3 +77,35 @@ R2[-1,:] = np.ones(n_s) * windowT
 
 # Will write algorithm to convert these room matrices into equation systems
 # that we solve to get the answer
+
+def make_small_room_eq_sys(room_matrix):
+    """
+    Calculates A and b from a room matrix
+    """
+    rows, cols = room_matrix.shape
+    u_rows = rows-1
+    u_cols = cols-2
+
+    # N unknowns in the equation system
+    n = (u_rows)*(u_cols)
+    b = np.zeros(n)
+    A = np.diag(np.ones(n)*-4)
+
+    ui = 0 # Unknown index
+
+    # This for loops puts all the ones in the matrix
+    for i in xrange(rows):
+        for j in xrange(cols):
+            if room_matrix[i][j] == 0: #Means it is an unknown
+                nearby = [(i+1,j),(i,j+1),(i-1,j),(i,j-1)]
+                for k in xrange(len(nearby)):
+                    ni,nj = nearby[k]
+                    if ni < rows and nj < cols: # Still inside matrix
+                        if room_matrix[ni][nj] == 0: # Nearby element is unknown
+                            ## Stoppa in etta i matrisen A
+                            ## Detta är sjuuuukt klurigt med indexen :D
+                        else:
+                            b[ui] -= room_matrix[ni][nj]
+                ui += 1
+
+    return A,b
