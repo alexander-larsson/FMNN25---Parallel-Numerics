c64d0d297263c813a64a3ab9ad57e24ce77c6057
Alexander Larsson <alexx.1992@gmail.com>
2015-10-20 14:16:23 +0200

    b matrix for big room

--- SingleCoreHeat2.py
+++ SingleCoreHeat2.py
@@ -49,8 +49,59 @@ def make_A_matrix_big_room(size):
     for i in xrange(A_size-n+2):
         A[i][i+n-2] = 1
         A[i+n-2][i] = 1
-
     return A
 
+
+def make_B_vector_big_room(left,top,right,bottom):
+    """
+    Parameters:
+    4 vectors corresponding to the elements on either side of the matrix.
+    If the matrix is of size n x m the top and bottom vectors will be n-2.
+    The left and right will be m-2.
+    For left and right 0 is the top element.
+    """
+    if not left.shape == right.shape or not top.shape == bottom.shape:
+        # Raise exception instead
+        print "Error, wrong shapes"
+    rows = left.size
+    cols = top.size
+
+    length = rows*cols
+
+    b = np.zeros(length)
+
+    # Hörn 1
+    b[0] = top[0]+left[0]
+
+    # rows - 2 element top
+    b[1:cols-1] = top[1:-1]
+
+    # Hörn 2
+    b[cols-1] = top[-1]+right[0]
+
+    # Mid points
+    for i in xrange(1,rows-1):
+        b[i*cols] = left[i]
+        b[i*cols+cols-1] = right[i]
+
+    # Hörn 3
+    b[length-cols] = bottom[0]+left[-1]
+
+    # rows - 2 element bottom
+    b[length-cols+1:-1] = bottom[1:-1]
+
+    # Hörn 4
+    b[length-1] = bottom[-1]+right[-1]
+
+    return b
+
+
+
+
 #print make_A_matrix_small_room(5,side="L")
-print make_A_matrix_big_room((7,4))
+#print make_A_matrix_big_room((7,4))
+l = np.ones(5)*15
+t = np.ones(3)*40
+b = np.ones(3)*5
+r = np.ones(5)*15
+print make_B_vector_big_room(l,t,r,b)
